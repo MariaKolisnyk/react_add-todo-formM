5d1d73ca2a4a28a766bd5998a0669ec86b4a8a0d
mar kol <example@e.com>
2024-10-02 14:24:54 +0200

    Updates

---
+++ precache-manifest.a4cd938eea5c3471073cfcd136f5c487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadf383147530c8cc53295c71fb8f740",
+    "url": "./index.html"
+  },
+  {
+    "revision": "562eb178c31290f11425",
+    "url": "./static/css/main.b66123a5.chunk.css"
+  },
+  {
+    "revision": "a65c5840591ef2441d81",
+    "url": "./static/js/2.b74a7e5f.chunk.js"
+  },
+  {
+    "revision": "075ee0fe56affc8020645735366f8cbd",
+    "url": "./static/js/2.b74a7e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562eb178c31290f11425",
+    "url": "./static/js/main.cf47553b.chunk.js"
+  },
+  {
+    "revision": "123b75263bfe1a4673ee",
+    "url": "./static/js/runtime-main.6f99edc8.js"
+  }
+]);
